112c25de635e96a4323e0c5669c4e2de609ec1bd
Dan7783 <spirdan19@gmail.comm>
2026-03-22 17:34:57 +1000

    added comments to supress not needed type checking warnings modified:   Trading_Implemntation/datamodel.py

--- Trading_Implemntation/datamodel.py
+++ Trading_Implemntation/datamodel.py
@@ -38,7 +38,7 @@ class Observation:
         self.conversionObservations = conversionObservations
         
     def __str__(self) -> str:
-        return "(plainValueObservations: " + jsonpickle.encode(self.plainValueObservations) + ", conversionObservations: " + jsonpickle.encode(self.conversionObservations) + ")"
+        return "(plainValueObservations: " + jsonpickle.encode(self.plainValueObservations) + ", conversionObservations: " + jsonpickle.encode(self.conversionObservations) + ")" # type: ignore
      
 
 class Order:
@@ -64,7 +64,7 @@ class OrderDepth:
 
 class Trade:
 
-    def __init__(self, symbol: Symbol, price: int, quantity: int, buyer: UserId=None, seller: UserId=None, timestamp: int=0) -> None:
+    def __init__(self, symbol: Symbol, price: int, quantity: int, buyer: UserId=None, seller: UserId=None, timestamp: int=0) -> None: # type: ignore
         self.symbol = symbol
         self.price: int = price
         self.quantity: int = quantity
